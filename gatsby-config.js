a05d2bd954d3b64d738f60e8b7990ff022dd33e5
boris <github@regmagik.com>
2023-08-27 11:55:08 -0400

    add sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,8 @@ module.exports = {
         icon: `src/images/regmagik.png`, // This path is relative to the root of the site.
       },
     },
-    //`gatsby-plugin-gatsby-cloud`,
+	`gatsby-plugin-sitemap`,
+    // `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
